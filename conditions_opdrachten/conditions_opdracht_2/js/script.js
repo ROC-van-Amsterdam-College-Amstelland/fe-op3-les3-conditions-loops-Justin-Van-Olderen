8e0cf5c1eb1a9026ef6b0673b6963bb6f460a240
Justin van Olderen <2107471@talnet.nl>
2019-12-03 14:51:48 +0100

    Opdracht 2 AF

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,15 @@
 //javascript code
+// function myFunction() {
+//     alert("Sorry, je hebt te weinig saldo!");
+//   }
+
+  function myFunction() {
+    var tekst;
+    var person = prompt("Please enter the ammount you want to withdraw:", "0");
+    if (person == null || person == "") {
+      tekst = "User cancelled the prompt.";
+    } else {
+      tekst = "The enterd ammount is" + person + "! We are unable to withdraw your request right now";
+    }
+    document.getElementById("demo").innerHTML = tekst;
+}
