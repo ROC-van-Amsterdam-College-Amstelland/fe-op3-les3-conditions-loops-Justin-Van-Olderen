a58cbd2f7d520651ce98d136c7d036ca1713b8c6
Justin van Olderen <2107471@talnet.nl>
2019-12-10 15:23:58 +0100

    Loops 1AF

--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+
+// var lijst = 0;
+
+// for( i=0, 1 < 5,i = 1)
+//     console.log(i);
+//     lijst = lijst + i;
+//     document.getElementById("output").innerHTML = i;
+
+//     if (condition) {
+        
+//     }
+
+// 
